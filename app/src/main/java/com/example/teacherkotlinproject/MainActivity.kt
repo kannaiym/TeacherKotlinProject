cfd3c9a6e55ca3ea5f145fc9f873500a9af6c7cd
ser1888 <ser1888kg@gmail.com>
2020-10-16 19:57:37 +0600

    hw 002

--- app/src/main/java/com/example/teacherkotlinproject/MainActivity.kt
+++ app/src/main/java/com/example/teacherkotlinproject/MainActivity.kt
@@ -2,12 +2,45 @@ package com.example.teacherkotlinproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var isMinus = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getResult()
+        setPlusOperator()
+        setMinusOperator()
+    }
+
+    private fun getResult() {
+        count_button.setOnClickListener {
+            val first = first_edit_text.text.toString().toInt()
+            val second = second_edit_text.text.toString().toInt()
+            var result = first + second
+
+            if (isMinus) result = first - second
+            else result = first + second
+
+            first_edit_text.text.clear()
+            second_edit_text.text.clear()
+            result_text_view.text = result.toString()
+        }
+    }
+
+    private fun setPlusOperator() {
+        minus_button.setOnClickListener {
+            isMinus = false
+        }
+    }
+
+    private fun setMinusOperator() {
+        minus_button.setOnClickListener {
+            isMinus = true
+        }
     }
 
 }
