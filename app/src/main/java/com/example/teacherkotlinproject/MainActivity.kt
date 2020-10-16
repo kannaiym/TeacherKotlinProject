5e43e8152c602b8b18c1774cf099cebf856a5556
ser1888 <ser1888kg@gmail.com>
2020-10-16 21:18:17 +0600

    lesson 003; array, for with examples

--- app/src/main/java/com/example/teacherkotlinproject/MainActivity.kt
+++ app/src/main/java/com/example/teacherkotlinproject/MainActivity.kt
@@ -1,46 +1,69 @@
 package com.example.teacherkotlinproject
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    // Сергей
-    // Адеми
-    //Айпери
-    //Азиза
-
-    //unmutable array
-//    var member = arrayOf("Сергей", "Адеми", "Айпери", "Азиза") //0, 1, 2, 3
-//    var memberMutable = mutableListOf("Сергей", "Адеми", "Айпери", "Азиза") //0, 1, 2, 3
-//    var ageArray = arrayOf<Int>()
-//    var ageMutable = mutableListOf<Int>()
-
-    public var total = 100
-    var animals = mutableListOf("cat", "dog", "bat")
+    /*
+    1. При добавлении элемента в массив сделать проверку на текст, текст не должен быть пустым.
+    2. При удалении элемента из массива делать проверку на пустой ли элемент и выводить Toast.
+     */
+    var textArray = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        displayAnimals()
+        addToArray()
+        removeFromArray()
     }
 
-    fun displayAnimals() {
-//        for (animal in animals) {
-//           Log.v("ANIMALS", animal)
-//        }
-//        Log.v("ANIMAL_AT", animals[2])
-//
-//        for (n in 1..9) {
-//            Log.v("RANGE", "step $n")
-//        }
-        var total = 100
-        while (total != 0) {
-            Log.v("TOTAL", "total is =$total")
-            total -= 1
+    private fun addToArray() {
+        add_button.setOnClickListener {
+            val value = enter_text_edit_text.text.toString()
+            enter_text_edit_text.text.clear()
+            textArray.add(value)
+            displayTextArray()
         }
+    }
 
+    private fun removeFromArray() {
+        remove_button.setOnClickListener {
+            var value = enter_text_delete_edit_text.text.toString()
+            enter_text_delete_edit_text.text.clear()
+            findAndRemoveFromArray(value)
+            displayTextArray()
+        }
+    }
+
+    private fun findAndRemoveFromArray(value: String) {
+        var indexOfArray: Int? = null
+        var element = ""
+        for ((i, text) in textArray.withIndex()) {
+            if (text == value) {
+                indexOfArray = i
+                element = text
+            }
+        }
+        if (indexOfArray != null ) {
+            textArray.removeAt(indexOfArray)
+            Toast.makeText(this, "Мы удалили $element", Toast.LENGTH_LONG).show()
+        }
+    }
+
+//    if (i != null ) {
+//        textArray.removeAt(i)
+//        Toast.makeText(this, toast, Toast.LENGTH_LONG).show()
+//    }
+
+//    if (i != null ) textArray.removeAt(i)
+
+    private fun displayTextArray() {
+        var result = ""
+        for (text in textArray) {
+            result = "$result $text \n"
+        }
+        all_elements_text_view.text = result
     }
 
 }
