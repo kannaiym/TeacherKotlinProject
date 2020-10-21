1af79c5871f6e00eaeb3f5cdf2e1e3d3cb93f879
ser1888 <ser1888kg@gmail.com>
2020-10-21 21:07:27 +0600

    lesson 005

--- app/src/main/java/com/example/teacherkotlinproject/MainActivity.kt
+++ app/src/main/java/com/example/teacherkotlinproject/MainActivity.kt
@@ -1,51 +1,31 @@
 package com.example.teacherkotlinproject
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.constrate_layout.*
 
 class MainActivity : AppCompatActivity() {
 
-    var name = "Sergey"
-    var lastname = "Karukes"
-    var age = "21"
+    //векторные картинки - svg, png
+    //растровые картинки - jpeg, jpg, gif, png
+
+    //Добавить ещё 3-4 картинки городов
+    //при нажатии на кнопку выбрать любую картинку из массива посредством класса Random
+
+    val imageArray = mutableListOf<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.constrate_layout)
-        setViews()
-        saveAction()
-        //MainActivity - camelCase - это для классов
-        //sizeOfShoes - camelCase - методов и переменных kotlin & java
-        //size_of_shoes - snake_case - ресурсов
+        changeIconAction()
     }
 
-    private fun setViews() {
-        username_value_text_view.text = name
-        lastname_value_text_view.text = lastname
-        age_value_text_view.text = age
-    }
-
-    private fun saveAction() {
-        save_button.setOnClickListener {
-            val phone = phone_edit_text.text.toString()
-            val city = city_edit_text.text.toString()
-            val region = region_edit_text.text.toString()
-
-            checkFields(phone, "Поле номер телефона не может быть пустым")
-            checkFields(city, "Поле города не может быть пустым")
-            checkFields(region, "Поле область не может быть пустым")
-
-            Log.v("SAVE_LOG", "$name, $lastname, $age, $phone, $city, $region")
-        }
-    }
-
-    private fun checkFields(value: String, message: String) {
-        if (value.isEmpty()) {
-            showToast(message)
-            return
+    private fun changeIconAction() {
+        change_image.setOnClickListener {
+            image.setImageResource(R.mipmap.paris)
+            val number = (0..1000).random()
+            showToast("Радомное число равно = $number")
         }
     }
 
