10f2580faadac09bed396cbca502fb4acc1ac3e8
ser1888 <ser1888kg@gmail.com>
2020-10-23 19:28:07 +0600

    hw 005

--- app/src/main/java/com/example/teacherkotlinproject/MainActivity.kt
+++ app/src/main/java/com/example/teacherkotlinproject/MainActivity.kt
@@ -7,13 +7,19 @@ import kotlinx.android.synthetic.main.constrate_layout.*
 
 class MainActivity : AppCompatActivity() {
 
-    //векторные картинки - svg, png
-    //растровые картинки - jpeg, jpg, gif, png
+    //val - константа, переменная, у которой нельзя поменять значение
+    //var - переменная, у которой можно поменять значение
+    val imageArray = mutableListOf<Int>().apply {
+        add(R.mipmap.paris)
+        add(R.mipmap.london)
+        add(R.mipmap.madrid)
+    }
 
-    //Добавить ещё 3-4 картинки городов
-    //при нажатии на кнопку выбрать любую картинку из массива посредством класса Random
-
-    val imageArray = mutableListOf<Int>()
+    val imageDescription = mutableListOf<String>().apply {
+        add("это paris")
+        add("это london")
+        add("это madrid")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +29,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun changeIconAction() {
         change_image.setOnClickListener {
-            image.setImageResource(R.mipmap.paris)
-            val number = (0..1000).random()
-            showToast("Радомное число равно = $number")
+            val random = imageArray.random()
+            image.setImageResource(random)
+            showToast(getIndexFromElement(random))
         }
+    }
+
+    private fun getIndexFromElement(element: Int): String {
+        var index: Int? = null
+        for ((i, item) in imageArray.withIndex()) {
+            if (item == element) index = i
+        }
+        return imageDescription[index!!]
     }
 
     private fun showToast(message: String) {
