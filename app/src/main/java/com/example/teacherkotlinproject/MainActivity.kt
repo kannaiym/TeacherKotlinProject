d3c229c04c2659dafdb17078f26efca5e3d6e5cd
ser1888 <ser1888kg@gmail.com>
2020-10-27 21:05:40 +0600

    lesson 7 end

--- app/src/main/java/com/example/teacherkotlinproject/MainActivity.kt
+++ app/src/main/java/com/example/teacherkotlinproject/MainActivity.kt
@@ -1,38 +1,51 @@
 package com.example.teacherkotlinproject
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    //HW 006
-    //При нажатии на кнопку передавать значение из EditText во 2-й активити и выводить Toast
-    //Каждый раз когда вы нажимаете на кнопку перейти на 2-й активити, то добавлять ваше значение в Array
-    //Добавить вторую кнопку в mainActivity, при нажатии на которую вы будете выводить все значения в Toast
+
+    //Сделать обработку всех кнопок чтобы от 0 до 9 можно было добавлять в EditText
+    //Сделать добавление символов / * + -
+    //Сделать очистку TextView на кнопку AC
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        nextActivityAction()
+        btn1()
+        btn2()
+        btn3()
     }
 
-    private fun nextActivityAction() {
-        go_to_next.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            val valueFromEditText = input.text.toString()
-            intent.putExtra("value", valueFromEditText)
-            startActivityForResult(intent, 1)
+    //здесь добавить 0
+
+    private fun btn1() {
+        btn_1.setOnClickListener {
+            var default = result.text.toString()
+            default += "1"
+            result.text = default
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (data == null) return
-        val modifyData = data.getStringExtra("modify_value")
-        input.setText(modifyData)
+    private fun btn2() {
+        btn_2.setOnClickListener {
+            var default = result.text.toString()
+            default += "2"
+            result.text = default
+        }
     }
+
+    private fun btn3() {
+        btn_3.setOnClickListener {
+            var default = result.text.toString()
+            default += "3"
+            result.text = default
+        }
+    }
+
+    //продолжите добавлять с 4
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
